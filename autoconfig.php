9e2e53519db419e8de14a29743cd2b3050701d16
tsgoff <tobias.sgoff@sgoff.com>
2014-11-27 16:33:04 +0100

    Delete autoconfig.php

--- autoconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-$AUTOCONFIG = array(
-  "dbtype"        => "mysql",
-  "dbname"        => "owncloud2",
-  "dbuser"        => "r00t",
-  "dbpass"        => "l33thaxor",
-  "dbhost"        => "mysql.cooper.sgoff.com",
-  "dbtableprefix" => "oc_",
-);
